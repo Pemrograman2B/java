61f5d226dc41ce3e126685570080139c697383e1
root <root@cuza>
2017-05-15 22:37:26 +0700

    Enkap e Boss

---
+++ Ktp.java
@@ -0,0 +1,112 @@
+
+public class Ktp {
+	private int NIK;
+	private String Nama;
+	private String Tem_La;
+	private String Jen_Kel;
+	private int Rt;
+	private int Rw;
+	private String Desa;
+	private String Kec;
+	private String Agama;
+	private String Status_Kawin;
+	private String Pekerjaan;
+	private String Kewarganegaraan;
+
+	public int getNIK() {
+		return NIK;
+	}
+
+	public void setNIK(int nIK) {
+		NIK = nIK;
+	}
+
+	public String getNama() {
+		return Nama;
+	}
+
+	public void setNama(String nama) {
+		Nama = nama;
+	}
+
+	public String getTem_La() {
+		return Tem_La;
+	}
+
+	public void setTem_La(String tem_La) {
+		Tem_La = tem_La;
+	}
+
+	public String getJen_Kel() {
+		return Jen_Kel;
+	}
+
+	public void setJen_Kel(String jen_Kel) {
+		Jen_Kel = jen_Kel;
+	}
+
+	public int getRt() {
+		return Rt;
+	}
+
+	public void setRt(int rt) {
+		Rt = rt;
+	}
+
+	public int getRw() {
+		return Rw;
+	}
+
+	public void setRw(int rw) {
+		Rw = rw;
+	}
+
+	public String getDesa() {
+		return Desa;
+	}
+
+	public void setDesa(String desa) {
+		Desa = desa;
+	}
+
+	public String getKec() {
+		return Kec;
+	}
+
+	public void setKec(String kec) {
+		Kec = kec;
+	}
+
+	public String getAgama() {
+		return Agama;
+	}
+
+	public void setAgama(String agama) {
+		Agama = agama;
+	}
+
+	public String getStatus_Kawin() {
+		return Status_Kawin;
+	}
+
+	public void setStatus_Kawin(String status_Kawin) {
+		Status_Kawin = status_Kawin;
+	}
+
+	public String getPekerjaan() {
+		return Pekerjaan;
+	}
+
+	public void setPekerjaan(String pekerjaan) {
+		Pekerjaan = pekerjaan;
+	}
+
+	public String getKewarganegaraan() {
+		return Kewarganegaraan;
+	}
+
+	public void setKewarganegaraan(String kewarganegaraan) {
+		Kewarganegaraan = kewarganegaraan;
+	}
+
+}
