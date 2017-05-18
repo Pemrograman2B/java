d28317cad1931d05a1ec5cbe6af9dd283cd63f28
root <root@cuza>
2017-05-18 21:57:06 +0700

    Coba arraylist (oxi 090)

--- DriverClass.java
+++ DriverClass.java
@@ -3,6 +3,8 @@ public class DriverClass {
 
 	public static void main(String[] args) {
 		ArrayList<Ktp> warga = new ArrayList<>();
+		
+		warga.add(new Ktp("123","Oxi","Blitar, 29 Oktober 199","Laki-Laki",2,4,"Ngrendeng","Selorejo","Islam","Belum Kawin","Mahasiswa","Indonesia"));
                 
 	}
 
