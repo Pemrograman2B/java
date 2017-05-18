8ee27a7f4282ff037f0b148f6230847fdd31559d
di2k <di2k@DESKTOP-5PFVFTS>
2017-05-18 21:04:52 +0700

    abc

--- JavaApplication20/src/javaapplication20/DriverClass.java
+++ JavaApplication20/src/javaapplication20/DriverClass.java
@@ -3,6 +3,7 @@ public class DriverClass {
 
 	public static void main(String[] args) {
 		          System.out.println("hello zidan");
+                          System.out.println("hai cantiik");
 
 	}
 
