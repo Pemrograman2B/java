c3c488796e4cbe5354b92fd0bcabb144e6e35034
mohammad zidan <zidandazizo@gmail.com>
2017-05-19 18:11:26 +0700

    Update DriverClass.java
    penggantian nama kecamatan dan kelurahan

--- DriverClass.java
+++ DriverClass.java
@@ -23,36 +23,26 @@ public class DriverClass {
 		// nambah orang
 		// int NIK
 		// ,nama,TTL,Age,JK,Alamat,StatusKawin,Kewarganegaraan,Agama,MasaBerlaku
-		                		Ktp gusti = new Ktp("20145564","Gusti Ayu","Blitar-1-September-1961","Perempuan",1,2,"Kepanjen","Lowokwaru","Islam","Kawin","Swasta","WNI");
+        Ktp gusti = new Ktp("20145564","Gusti Ayu","Blitar-1-September-1961","Perempuan",1,2,"Kepanjen","Lowokwaru","Islam","Kawin","Swasta","WNI");
 		Ktp mari = new Ktp("20147546", "Marilanda", "Samarinda-3-Agustus-1995", "Perempuan", 2, 21, "Paser", "Balikpapan", "Kristen","Belum Kawin","Wiraswasta","WNI");
 		Ktp ande = new Ktp("20147783","Anderia","Jombang-18-Mei-1977","Perempuan",1,9,"Kademangan","Blitar","Islam","Kawin","Swasta","WNI");
 		Ktp syaiful = new Ktp("20157789","Syaifullah","Jakarta,9-Januari-1967","Laki-laki",3,1,"Sampit","Lowokwaru","Islam","Belum Kawin","Swasta","WNI");	
 		Ktp oka = new Ktp("20179975","Oka Berliano","Blitar-7-Januari-1998","Laki-laki",4,5,"Jone","Tanah Grogot","Islam","Kawin","Swasta","WNI");
 		Ktp farid = new Ktp("20118896","Farid Fatullah","Madura-14-Februari-1998","Laki-laki",5,5,"Pademawu","Pamekasan","Islam","Kawin","Swasta","WNI");
-                Ktp fatma = new Ktp("20176664","Fatmawati","Ambon-25-November-1998","Perempuan",6,12,"Waiheru","Baguala","Budha","Kawin","Mahasiswa","WNI");
-                Ktp ditha = new Ktp("20178943","Ditha Ameilia","Situbondo-8-Mei-1998","Perempuan",7,15,"Awar-awar","Kuamki Baru","Islam","Belum Kawin","Mahasiswa","WNI");
-                Ktp rizka = new Ktp("20145337","Rizka Mouliani","Tanjungpinang-25-Agustus-1998","Perempuan",8,89,"Rejowinangun","Kademangan","Islam","Belum Kawin","Swasta","WNI");
-                Ktp fina = new Ktp("20178894","Fina Afifana","Pamekasan-9-April-1997","Perempuan",1,15,"Gurem","Pamekasan","Islam","Kawin","PNS","WNI");
-                Ktp rena = new Ktp("2016103703", "Rena putri", "Mojokerto, 14-April-1998", "Perempuan", 2, 1, "Kutoporong", "Bangsal", "ISLAM", "Menikah",
-				"Mahasiswa", "WNI");
-		Ktp wicak = new Ktp("2016103403", "Wicaksono Prawiro", "Mojokerto, 17-Januari-1991", "Laki-Laki", 5, 2, "Jabon", "Mojoanyar", "ISLAM", "Menikah",
-				"Wiraswasta", "WNI");
-		Ktp kevin = new Ktp("2016103503", "Kevin Adinata", "Bandung, 15-Oktober-1997", "Laki-Laki", 16, 9, "Gayam", "Bangsal", "ISLAM", "Belum Menikah",
-				"PNS", "WNI");
-		Ktp finis = new Ktp("2016103603", "Finishtri Demiasih", "Aceh, 15-Mei-1994", "Perempuan", 13, 5, "Gebangmalang", "Mojoanyar", "ISLAM", "Cerai",
-				"Wiraswasta", "WNI");
-		Ktp ou = new Ktp("2016103803", "Ourievia Stevani", "Sidoarjo, 6-Februari-1993", "Perempuan", 5, 4, "Bukittinggi", "Banuhampu", "ISLAM", "Menikah",
-				"Ibu rumah tangga", "WNI");
-		Ktp gladis = new Ktp("2016103903", "Gladiska Fristia", "Trenggalek, 28-Februari-1991", "Perempuan", 6, 3, "PandaiSingkek", "Kubang Putiah", "ISLAM", "Belum Menikah",
-				"Mahasiswa", "WNI");
-		Ktp ray = new Ktp("2016103104", "Rayhan", "Cipanas, 8-Maret-1995", "Laki-Laki", 17, 8, "Tirto", "Landungsari", "ISLAM", "Belum Menikah",
-				"Mahasiswa", "WNI");
-		Ktp nia = new Ktp("2016110330", "Nia Dwi", "Balikpapan, 19-Oktober-1997", "Perempuan", 22, 5, "Jone", "Tanah Grogot", "ISLAM", "Belum Menikah",
-				"Mahasiswa", "WNI");
-		Ktp danker = new Ktp("20161110220", "Danker Arkeb", "Padang, 19-September-1997", "Laki-Laki", 1, 1, "Keboan", "Meri", "ISLAM", "Belum Menikah",
-				"Wiraswasta", "WNI");
-		Ktp putra = new Ktp("2016110440", "Putra Ardiansyah", "Madura, 17-Maret-1994", "Laki-Laki", 7, 5, "Puloniti", "Bangsal", "ISLAM", "Menikah",
-				"PNS", "WNI");
+        Ktp fatma = new Ktp("20176664","Fatmawati","Ambon-25-November-1998","Perempuan",6,12,"Waiheru","Baguala","Budha","Kawin","Mahasiswa","WNI");
+        Ktp ditha = new Ktp("20178943","Ditha Ameilia","Situbondo-8-Mei-1998","Perempuan",7,15,"Awar-awar","Kuamki Baru","Islam","Belum Kawin","Mahasiswa","WNI");
+        Ktp rizka = new Ktp("20145337","Rizka Mouliani","Tanjungpinang-25-Agustus-1998","Perempuan",8,89,"Rejowinangun","Kademangan","Islam","Belum Kawin","Swasta","WNI");
+        Ktp fina = new Ktp("20178894","Fina Afifana","Pamekasan-9-April-1997","Perempuan",1,15,"Gurem","Pamekasan","Islam","Kawin","PNS","WNI");
+        Ktp rena = new Ktp("2016103703", "Rena putri", "Mojokerto, 14-April-1998", "Perempuan", 2, 1, "Kutoporong", "Bangsal", "ISLAM", "Menikah","Mahasiswa", "WNI");
+		Ktp wicak = new Ktp("2016103403", "Wicaksono Prawiro", "Mojokerto, 17-Januari-1991", "Laki-Laki", 1, 2, "Jabon", "Mojoanyar", "ISLAM", "Menikah","Wiraswasta", "WNI");
+		Ktp kevin = new Ktp("2016103503", "Kevin Adinata", "Bandung, 15-Oktober-1997", "Laki-Laki", 2, 9, "Gayam", "Bangsal", "ISLAM", "Belum Menikah","PNS", "WNI");
+		Ktp finis = new Ktp("2016103603", "Finishtri Demiasih", "Aceh, 15-Mei-1994", "Perempuan", 3, 5, "Gebangmalang", "Mojoanyar", "ISLAM", "Cerai","Wiraswasta", "WNI");
+		Ktp ou = new Ktp("2016103803", "Ourievia Stevani", "Sidoarjo, 6-Februari-1993", "Perempuan", 4, 4, "Bukittinggi", "Banuhampu", "ISLAM", "Menikah","Ibu rumah tangga", "WNI");
+		Ktp gladis = new Ktp("2016103903", "Gladiska Fristia", "Trenggalek, 28-Februari-1991", "Perempuan", 5, 3, "PandaiSingkek", "Kubang Putiah", "ISLAM", "Belum Menikah","Mahasiswa", "WNI");
+		Ktp ray = new Ktp("2016103104", "Rayhan", "Cipanas, 8-Maret-1995", "Laki-Laki", 6, 8, "Tirto", "Landungsari", "ISLAM", "Belum Menikah","Mahasiswa", "WNI");
+		Ktp nia = new Ktp("2016110330", "Nia Dwi", "Balikpapan, 19-Oktober-1997", "Perempuan", 7, 5, "Jone", "Tanah Grogot", "ISLAM", "Belum Menikah","Mahasiswa", "WNI");
+		Ktp danker = new Ktp("20161110220", "Danker Arkeb", "Padang, 19-September-1997", "Laki-Laki", 8, 1, "Keboan", "Meri", "ISLAM", "Belum Menikah","Wiraswasta", "WNI");
+		Ktp putra = new Ktp("2016110440", "Putra Ardiansyah", "Madura, 17-Maret-1994", "Laki-Laki", 1, 5, "Puloniti", "Bangsal", "ISLAM", "Menikah","PNS", "WNI");
 		
 		// masukkan orang ke RT tertentu
 		RT1p.put(gusti.getNama(), gusti);
@@ -65,17 +55,17 @@ public class DriverClass {
 		RT4p.put(ditha.getNama(), ditha);
 		RT4p.put(rizka.getNama(), rizka);
 		RT4p.put(fina.getNama(), fina);
-              	RT1p.put(rena.getNama(), rena);
+                RT2p.put(rena.getNama(), rena);
 		RT1p.put(wicak.getNama(), wicak);
 		RT2p.put(kevin.getNama(), kevin);
-		RT2p.put(finis.getNama(), finis);
-		RT3p.put(ou.getNama(), ou);
-		RT3p.put(gladis.getNama(), gladis);
-		RT3p.put(ray.getNama(), ray);
-		RT4p.put(nia.getNama(), nia);
-		RT4p.put(danker.getNama(), danker);
-		RT4p.put(putra.getNama(), putra);
-
+		RT3p.put(finis.getNama(), finis);
+		RT4p.put(ou.getNama(), ou);
+		RT5p.put(gladis.getNama(), gladis);
+		RT6p.put(ray.getNama(), ray);
+		RT7p.put(nia.getNama(), nia);
+		RT8p.put(danker.getNama(), danker);
+		RT1p.put(putra.getNama(), putra);
+                
 		// Masukkin er Arraylist untuk sorting
 		List<Ktp> RT1 = new ArrayList<>(RT1p.values());
 		List<Ktp> RT2 = new ArrayList<>(RT2p.values());
@@ -90,15 +80,15 @@ public class DriverClass {
 
 		// menampilkan
 		int i;
-		System.out.println("DATA PENDUDUK KECAMATAN BALIKPAPAN BARAT");
+		System.out.println("DATA PENDUDUK KECAMATAN NGALAM BRO");
 		System.out.println("Daftar Kelurahan    :");
-		System.out.println("1.Kelurahan Margo Mulyo\n2.Kelurahan Kampung Baru Ulu");
+		System.out.println("1.Kelurahan Merjosari\n2.Kelurahan Tirto utomo");
 		System.out.println("\nMasukkan Pilihan : ");
 		i = input.nextInt();
 
 		// kelurahan Margo Mulyo
 		if (i == 1) {
-			System.out.println("Daftar RT di Kelurahan Margo Mulyo:");
+			System.out.println("Daftar RT di Kelurahan Merjosari:");
 			System.out.println("1.RT 1\n2.RT 2\n3.RT 3\n4.RT 4");
 			System.out.println("\nMasukkan Pilihan : ");
 			int rta = input.nextInt();
@@ -136,7 +126,7 @@ public class DriverClass {
 
 		// kelurahan Kampung Baru Ulu
 		else if (i == 2) {
-			System.out.println("Daftar RT di Kelurahan Kampung Baru Ulu");
+			System.out.println("Daftar RT di Kelurahan Tirto utomo");
 			System.out.println("1.RT 5\n2.RT 6\n3.RT 7\n4.RT 8");
 			System.out.println("\nMasukkan Pilihan : ");
 			int rta = input.nextInt();
@@ -149,7 +139,7 @@ public class DriverClass {
 							+ p.getKewarganegaraan());
 				});
 			} else if (rta == 2) {
-				RT6.forEach((p) -> {
+				RT6.forEach((p) -> { 
 					System.out.println(p.getNIK() + "\t" + p.getNama() + "\t" + p.getTTL() + "\t" + p.getJK() + "\t"
 							+ p.getRt() + "\t" + p.getRw() + "\t" + p.getDesa() + "\t" + p.getKec() + "\t"
 							+ p.getAgama() + "\t" + p.getStatus_Kawin() + "\t" + p.getPekerjaan() + "\t"
